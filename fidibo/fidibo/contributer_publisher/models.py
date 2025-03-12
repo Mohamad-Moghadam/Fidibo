529d608ed011f413a1e9f755ec103f30342dce15
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-12 16:30:45 +0330

    🎨 adding the final features to the classes

--- fidibo/fidibo/contributer_publisher/models.py
+++ fidibo/fidibo/contributer_publisher/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-from books_magazines.models import Ebooks
+from books_magazines.models import Ebooks, Audiobook, Magazines
 
 class Contributers(models.Model):
     name = models.CharField(max_length = 100)
-    books = models.ForeignKey(to = Ebooks, on_delete=models.CASCADE, related_name = "books_of_the_author")
+    ebooks = models.ForeignKey(to = Ebooks, on_delete=models.CASCADE, related_name = "ebooks_of_the_author")
+    audiobooks = models.ForeignKey(to = Audiobook, on_delete=models.CASCADE, related_name = "audiobooks_of_the_author")
     biography = models.TextField()
 
 class Publishers(models.Model):
     name = models.CharField(max_length = 100)
-    books = models.ForeignKey(to = Ebooks, on_delete=models.CASCADE, related_name = "books_of_the_author")
+    ebooks = models.ForeignKey(to = Ebooks, on_delete=models.CASCADE, related_name = "ebooks_of_the_publisher")
+    audiobooks = models.ForeignKey(to = Audiobook, on_delete=models.CASCADE, related_name = "audiobooks_of_the_publisher")
+    Magazines = models.ForeignKey(to = Magazines, on_delete=models.CASCADE, related_name = "magazines_of_the_publisher")
     biography = models.TextField()
