0c59c0bf3e7cd15b9d0a8ef65141f4757ad7f170
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-22 00:37:00 +0330

    ✨ new view added for adding magazines

--- fidibo/fidibo/books_magazines/views.py
+++ fidibo/fidibo/books_magazines/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-from books_magazines.models import Ebooks,Audiobook
+from books_magazines.models import Ebooks, Audiobook, Magazines
 
 
 def ebooks(request, category: str):
@@ -48,6 +48,20 @@ def add_book(request):
                 publisher = data.get("publisher"),
                 date_of_publish = data.get("date_of_publish"),
                 narrator = data.get("narrator"),
+                describtion = data.get("describtion"),
+                language = data.get("language"),
+                volume = data.get("volume"),
+            )
+
+        elif data.get("type") == "magazine":
+            Magazines.objects.create(
+                name = data.get("name"),
+                author = data.get("author"),
+                price = data.get("price"),
+                rating = data.get("rating"),
+                publisher = data.get("publisher"),
+                time_to_read = data.get("time_to_read"),
+                date_of_publish = data.get("date_of_publish"),
                 describtion = data.get("describtion"),
                 language = data.get("language"),
                 volume = data.get("volume"),
